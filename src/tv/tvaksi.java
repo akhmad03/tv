33dc5670d3af34207679e5004302d25157663aaf
akhmad03 <37820079+akhmad03@users.noreply.github.com>
2018-03-27 11:19:47 +0800

    mengisi nilai atribut

--- src/tv/tvaksi.java
+++ src/tv/tvaksi.java
@@ -14,6 +14,26 @@ public class tvaksi {
         class_tv adam = new class_tv();
         class_tv memed = new class_tv();
         class_tv ujang = new class_tv();
-    
+        
+        System.out.println("tv adam");
+        adam.merek="samsung";
+        adam.harga=10000000;
+        adam.jenis="LCD";
+        adam.ukuran="21 inch";
+        adam.warna="hitam";
+        
+        System.out.println("tv memed");
+        memed.merek="LG";
+        memed.harga=15000000;
+        memed.jenis="LED";
+        memed.ukuran="31 inch";
+        memed.warna="putih";
+        
+        System.out.println("tv ujang");
+        ujang.merek="Toshiba";
+        ujang.harga=10000000;
+        ujang.jenis="LED";
+        ujang.ukuran="24 inch";
+        ujang.warna="silver";
     }
 }
